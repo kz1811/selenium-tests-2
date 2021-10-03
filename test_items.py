2cb131a72e9bb4d7540a6329ec6d3d364ce69510
Your Name <you@example.com>
2021-10-03 23:52:12 +0300

    correct name of test

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
 
-def test_item(browser):
+def test_button_add_to_basket_is_here(browser):
     link =  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button_add_to_basket = browser.find_element(By.CLASS_NAME, "add-to-basket")
