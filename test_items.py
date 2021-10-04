f8fd36ec2d3855387947b2ba55364ca526fa3d3a
Your Name <you@example.com>
2021-10-04 19:56:15 +0300

    Fix assert

--- test_items.py
+++ test_items.py
@@ -5,6 +5,4 @@ import time
 def test_button_add_to_basket_is_here(browser):
     link =  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button_add_to_basket = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    time.sleep(10)
-    assert button_add_to_basket, "Should be 'Add to basket' button"
+    assert browser.find_elements(By.CLASS_NAME, "btn-ad-to-basket"), "Should be 'Add to basket' button"
