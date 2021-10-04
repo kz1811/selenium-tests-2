e73718f190f111ccd8198a50f53a07ba777dd5d8
Your Name <you@example.com>
2021-10-04 19:57:11 +0300

    fix locator

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ import time
 def test_button_add_to_basket_is_here(browser):
     link =  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    assert browser.find_elements(By.CLASS_NAME, "btn-ad-to-basket"), "Should be 'Add to basket' button"
+    assert browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"), "Should be 'Add to basket' button"
